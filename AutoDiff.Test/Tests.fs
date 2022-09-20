843dce18a1fc8805080c032c78c2206f193b8b76
Brian Berns <brianberns@gmail.com>
2022-09-20 11:08:06 -0400

    Renamed tests.

--- AutoDiff.Test/Tests.fs
+++ AutoDiff.Test/Tests.fs
@@ -7,7 +7,7 @@ open AutoDiff
 type TestClass () =
 
     [<TestMethod>]
-    member _.Test1() =
+    member _.Basic() =
 
         let inline f1 z =
             sqrt (Generic.fromInt 3 * sin z)
@@ -27,7 +27,7 @@ type TestClass () =
         Assert.AreEqual(dual2, dual1)
 
     [<TestMethod>]
-    member _.Test2() =
+    member _.Polynomial() =
 
         let inline f x =
             let g2 = Generic.fromInt 2
@@ -38,18 +38,13 @@ type TestClass () =
         let valf = f (D (10.0, 1.0))
         Assert.AreEqual<_>(664.0, valf.Deriv)
 
+    /// Differentiate high-dimensional function.
     [<TestMethod>]
-    member _.Test3() =
+    member _.HighDimension() =
 
         let inline f x y z =   // f: R^3 -> R
             Generic.fromInt 2 * x**2 + Generic.fromInt 3 * y + sin z
-        let dual1 = f (D (3., 1.)) (D (4., 1.)) (D (5., 1.))   // call f with dual numbers, set derivative to 1
-        Assert.AreEqual<_>(29.04107572533686, dual1.Value)
-        Assert.AreEqual<_>(15.283662185463227, dual1.Deriv)
 
-        (*
-        D 29.041077 15.283662
-        > f x y z = (2 * x^2, 3 * y + sin z)       -- f: R^3 -> R^2
-        > f (D 3 1) (D 4 1) (D 5 1) :: (Dual Float', Dual Float')
-        (D 18.0 12.0,D 11.041076 3.2836623)
-        *)
+        let dual = f (D (3., 1.)) (D (4., 1.)) (D (5., 1.))   // call f with dual numbers, set derivative to 1
+        Assert.AreEqual<_>(29.04107572533686, dual.Value)
+        Assert.AreEqual<_>(15.283662185463227, dual.Deriv)
