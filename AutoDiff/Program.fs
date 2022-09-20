b6cdc1ced0005c69477dba736c4b3f320cd762f8
Brian Berns <brianberns@gmail.com>
2022-09-20 01:25:55 -0400

    Exponentiation.

--- AutoDiff/Program.fs
+++ AutoDiff/Program.fs
@@ -28,6 +28,9 @@ type D<'a
     static member inline (*)(D (x, x'), D (y, y')) =
         D (x * y, y' * x + x' * y)
 
+    static member inline (/)(D (x, x'), D (y, y')) =
+        failwith "moo"
+
     static member inline (~-)(D (x, x')) =
         D (-x, -x')
 
@@ -44,9 +47,25 @@ let inline constD x = D (x, GenericZero)
 let inline idD x = D (x, GenericOne)
 
 let inline f1 z =
-    // sqrt ((GenericOne + GenericOne + GenericOne) * sin z)
     sqrt ((Generic.fromInt 3) * sin z)
 
-let d = D (2.0, 1.0)
-let moo = f1 d
-printfn "%A" moo
+let val1 = f1 (D (2.0, 1.0))
+printfn "%A" val1
+
+let val1f = f1 (D (2.0f, 1.0f))
+printfn "%A" val1f
+
+let inline f2 x =
+    D (f1 x, (3.0 * cos x / (2.0 * sqrt (3.0 * sin x))))
+
+let val2 = f2 2.0
+printfn "%A" val2
+
+let inline f x =
+    let g2 = Generic.fromInt 2
+    let g3 = Generic.fromInt 3
+    let g4 = Generic.fromInt 4
+    g2 * pown x 3 + g3 * pown x 2 + g4 * x + g2
+
+let valf = f (D (10.0, 1.0))
+printfn "%A" valf
