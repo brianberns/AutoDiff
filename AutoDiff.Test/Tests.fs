f2dd6ebed584d74ba7aa28b239436bc532d312a1
Brian Berns <brianberns@gmail.com>
2022-09-20 11:15:25 -0400

    Enhanced test.

--- AutoDiff.Test/Tests.fs
+++ AutoDiff.Test/Tests.fs
@@ -12,16 +12,20 @@ type TestClass () =
         let inline f1 z =
             sqrt (Generic.fromInt 3 * sin z)
 
+        // expected value and derivative at x = 2
+        let expectedValue = 1.6516332160855343
+        let expectedDeriv = -0.3779412091869595
+
         let val1 = f1 2.0
-        Assert.AreEqual<_>(1.6516332160855343, val1)
+        Assert.AreEqual<_>(expectedValue, val1)
 
         let dual1 = f1 (D (2.0, 1.0))
-        Assert.AreEqual<_>(1.6516332160855343, dual1.Value)
-        Assert.AreEqual<_>(-0.3779412091869595, dual1.Deriv)
+        Assert.AreEqual<_>(expectedValue, dual1.Value)
+        Assert.AreEqual<_>(expectedDeriv, dual1.Deriv)
 
         let dual1f = f1 (D (2.0f, 1.0f))
-        Assert.AreEqual(1.6516332160855343f, dual1f.Value, 0.000001f)
-        Assert.AreEqual(-0.3779412091869595f, dual1f.Deriv, 0.000001f)
+        Assert.AreEqual(float32 expectedValue, dual1f.Value, 0.000001f)
+        Assert.AreEqual(float32 expectedDeriv, dual1f.Deriv, 0.000001f)
 
         let inline f2 x =
             D (f1 x, (3.0 * cos x / (2.0 * sqrt (3.0 * sin x))))
